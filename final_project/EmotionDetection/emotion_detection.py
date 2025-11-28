43a6a3dec135c823a14689f4aac9bd459a876365
Isi-dev <isinsemail@gmail.com>
2025-11-28 16:36:22 -0500

    Included error handling

--- final_project/EmotionDetection/emotion_detection.py
+++ final_project/EmotionDetection/emotion_detection.py
@@ -4,14 +4,20 @@ Function to detect emotions using the Watson NLP library
 
 import requests, json
 
-def emotion_detector(text_to_analyse):
+def emotion_detector(text_to_analyse=""):
     """
     This function enables communication with the Watson API for emotion detection
     """
+    if not text_to_analyse or text_to_analyse.strip() == "":
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = {"raw_document": {"text": text_to_analyse}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
     response = requests.post(url, json=myobj, headers=header)
+    if response.status_code == 400:
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+
     formatted_response = json.loads(response.text)
 
     # Extract emotion scores
