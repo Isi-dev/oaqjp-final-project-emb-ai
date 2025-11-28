c5ed7ddff3ee07f62bb28ada5fe812475206758e
Isi-dev <isinsemail@gmail.com>
2025-11-28 16:50:41 -0500

    static code analysis complete. last commit.

--- final_project/server.py
+++ final_project/server.py
@@ -26,10 +26,18 @@ def emo_detector():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+        f"'joy': {joy}, and 'sadness': {sadness}'. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
 
 @app.route("/")
 def render_index_page():
+    """
+    Render the main page
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
